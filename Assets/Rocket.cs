84f3924c4aa666a5579c6bcf7c00401a50f5eaca
Frédéric Darré <frederic.darre@outlook.com>
2020-05-01 09:59:30 +0200

    Add thrusting to rocket when pushing space

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidBody; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>(); //access the rigidBody of the rocket
     }
 
     // Update is called once per frame
@@ -23,7 +25,11 @@ public class Rocket : MonoBehaviour
 
         if (Input.GetKey(KeyCode.Space))  //can thrust while rotating
         {
-            print("Thrusting");
+            //add force to the rigidbody relative to its coordinate system
+            //will add force that is always in the direction that the ship is facing
+            // the function want a vector3
+            //3 floating point position numbers bundeled together
+            rigidBody.AddRelativeForce(Vector3.up);
         }
     
         if (Input.GetKey(KeyCode.Q))
